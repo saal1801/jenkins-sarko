a0672a0efaf3e51d9267ff6101cc88b4e8891d70
nikalsh <niklas@helander.se>
2019-09-03 13:21:56 +0200

    pupdate person

--- src/main/java/example/Person.java
+++ src/main/java/example/Person.java
@@ -7,5 +7,50 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class Person {
+    private String name;
+    private String nationality;
+    private String ehtnicity;
+    private String birthdate;
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getEhtnicity() {
+        return ehtnicity;
+    }
+
+    public void setEhtnicity(String ehtnicity) {
+        this.ehtnicity = ehtnicity;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 
 }
